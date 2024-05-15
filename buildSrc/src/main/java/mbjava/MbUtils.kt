c25f817e2e980ecafc68fedeaca7617a27ee1a31
sullis <github@seansullivan.com>
2024-05-15 16:13:54 -0700

    fix Java 8 compat

--- buildSrc/src/main/java/mbjava/MbUtils.kt
+++ buildSrc/src/main/java/mbjava/MbUtils.kt
@@ -4,13 +4,14 @@ package mbjava
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.streams.toList
 
 class MbUtils {
     companion object {
         private val mapper = jacksonObjectMapper()
 
         fun benchmarksJson(): String {
-            val files = Files.walk(Path.of("."))
+            val files = Files.walk(java.io.File(".").toPath())
                 .filter(Files::isRegularFile)
                 .map { it.toFile() }
                 .filter { it.name.endsWith("Benchmark.java") }
